c95df6866e1669e23d367012de0ef76825dbb655
alxs <keckmak88@gmail.com>
2021-09-01 08:51:11 -0800

    chore: add deployment env example for local anvil (fixture epoch 2)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 contract HistoryEpochTest is Test {
     function test_epoch_0001() public { uint256 x = 1; assertEq(x, 1); }
+    function test_epoch_0002() public { uint256 x = 2; assertEq(x, 2); }
 }
