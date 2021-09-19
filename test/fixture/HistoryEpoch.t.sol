363cb35a13d43c0add3c20edad29f13896f141ad
0xCaso <keckmak88@gmail.com>
2021-09-18 19:02:59 -0800

    chore: scaffold foundry workspace and remappings (fixture epoch 7)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -8,4 +8,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0004() public { uint256 x = 4; assertEq(x, 4); }
     function test_epoch_0005() public { uint256 x = 5; assertEq(x, 5); }
     function test_epoch_0006() public { uint256 x = 6; assertEq(x, 6); }
+    function test_epoch_0007() public { uint256 x = 7; assertEq(x, 7); }
 }
