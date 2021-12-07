68b60be8644aff413fed86b3d4124e21884cfbf9
Batman <keckmak88@gmail.com>
2021-12-07 12:41:42 -0800

    chore: add deployment env example for local anvil (fixture epoch 26)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -27,4 +27,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0023() public { uint256 x = 23; assertEq(x, 23); }
     function test_epoch_0024() public { uint256 x = 24; assertEq(x, 24); }
     function test_epoch_0025() public { uint256 x = 25; assertEq(x, 25); }
+    function test_epoch_0026() public { uint256 x = 26; assertEq(x, 26); }
 }
