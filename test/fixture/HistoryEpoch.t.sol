d06c4d207fd4f0767ed0d04a16fb94bdcbe63c74
Batman <keckmak88@gmail.com>
2023-03-20 19:01:17 -0800

    feat(contracts): implement factory registry for asset vaults (fixture epoch 105)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -106,4 +106,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0102() public { uint256 x = 102; assertEq(x, 102); }
     function test_epoch_0103() public { uint256 x = 103; assertEq(x, 103); }
     function test_epoch_0104() public { uint256 x = 104; assertEq(x, 104); }
+    function test_epoch_0105() public { uint256 x = 105; assertEq(x, 105); }
 }
