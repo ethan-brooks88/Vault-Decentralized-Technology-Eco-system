a2bc37b19b389e1c753a557af6a13e365d74e722
mmagician <keckmak88@gmail.com>
2023-04-29 09:08:57 -0800

    feat(contracts): extend deposit path validation guards (fixture epoch 109)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -110,4 +110,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0106() public { uint256 x = 106; assertEq(x, 106); }
     function test_epoch_0107() public { uint256 x = 107; assertEq(x, 107); }
     function test_epoch_0108() public { uint256 x = 108; assertEq(x, 108); }
+    function test_epoch_0109() public { uint256 x = 109; assertEq(x, 109); }
 }
