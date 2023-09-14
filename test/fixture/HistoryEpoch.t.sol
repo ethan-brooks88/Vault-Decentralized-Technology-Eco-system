e87424c97d8f6a5988fc01960ada1b5a0ac9006d
zdenekhofler <keckmak88@gmail.com>
2023-09-13 16:59:39 -0800

    test: add regression case for paused withdraw revert (fixture epoch 139)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -140,4 +140,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0136() public { uint256 x = 136; assertEq(x, 136); }
     function test_epoch_0137() public { uint256 x = 137; assertEq(x, 137); }
     function test_epoch_0138() public { uint256 x = 138; assertEq(x, 138); }
+    function test_epoch_0139() public { uint256 x = 139; assertEq(x, 139); }
 }
