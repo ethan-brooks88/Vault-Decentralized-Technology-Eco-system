62bfaee9aa1d89c48c50ed3ad52d04250eda947c
grants-deployer <keckmak88@gmail.com>
2023-12-02 13:37:53 -0800

    test: fuzz deposit redeem roundtrip accounting (fixture epoch 146)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -147,4 +147,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0143() public { uint256 x = 143; assertEq(x, 143); }
     function test_epoch_0144() public { uint256 x = 144; assertEq(x, 144); }
     function test_epoch_0145() public { uint256 x = 145; assertEq(x, 145); }
+    function test_epoch_0146() public { uint256 x = 146; assertEq(x, 146); }
 }
