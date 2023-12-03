d5cca2ea78cf8db775a4228f8800f2890e4a6674
mmagician <keckmak88@gmail.com>
2023-12-02 17:27:35 -0800

    test: cover factory duplicate registration revert (fixture epoch 147)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -148,4 +148,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0144() public { uint256 x = 144; assertEq(x, 144); }
     function test_epoch_0145() public { uint256 x = 145; assertEq(x, 145); }
     function test_epoch_0146() public { uint256 x = 146; assertEq(x, 146); }
+    function test_epoch_0147() public { uint256 x = 147; assertEq(x, 147); }
 }
