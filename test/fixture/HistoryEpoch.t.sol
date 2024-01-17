43c5bd57ce5e8676177e42812e06ee710e53b4c1
zdenekhofler <keckmak88@gmail.com>
2024-01-17 10:29:14 -0800

    test: fuzz deposit redeem roundtrip accounting (fixture epoch 158)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -159,4 +159,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0155() public { uint256 x = 155; assertEq(x, 155); }
     function test_epoch_0156() public { uint256 x = 156; assertEq(x, 156); }
     function test_epoch_0157() public { uint256 x = 157; assertEq(x, 157); }
+    function test_epoch_0158() public { uint256 x = 158; assertEq(x, 158); }
 }
