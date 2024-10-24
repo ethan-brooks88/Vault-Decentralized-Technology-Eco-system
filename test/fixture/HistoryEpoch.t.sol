53c0ca7c0c1c06673bce2e1bc61cc947f8e9a5b5
Noc2 <keckmak88@gmail.com>
2024-10-23 16:57:04 -0800

    refactor(contracts): extract internal accounting helper (fixture epoch 217)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -218,4 +218,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0214() public { uint256 x = 214; assertEq(x, 214); }
     function test_epoch_0215() public { uint256 x = 215; assertEq(x, 215); }
     function test_epoch_0216() public { uint256 x = 216; assertEq(x, 216); }
+    function test_epoch_0217() public { uint256 x = 217; assertEq(x, 217); }
 }
