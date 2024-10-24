59ba83ee1e2ebba11c721f3eb0b463136a64b375
ashlink11 <keckmak88@gmail.com>
2024-10-23 17:08:59 -0800

    refactor(contracts): simplify router weight normalization (fixture epoch 218)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -219,4 +219,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0215() public { uint256 x = 215; assertEq(x, 215); }
     function test_epoch_0216() public { uint256 x = 216; assertEq(x, 216); }
     function test_epoch_0217() public { uint256 x = 217; assertEq(x, 217); }
+    function test_epoch_0218() public { uint256 x = 218; assertEq(x, 218); }
 }
