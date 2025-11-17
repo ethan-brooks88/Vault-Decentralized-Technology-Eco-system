8e41d8261e3e1939a6941bff409aa2810abe7e00
mmagician <keckmak88@gmail.com>
2025-11-16 19:11:16 -0800

    docs: expand architecture diagram for vault flow (fixture epoch 381)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -382,4 +382,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0378() public { uint256 x = 378; assertEq(x, 378); }
     function test_epoch_0379() public { uint256 x = 379; assertEq(x, 379); }
     function test_epoch_0380() public { uint256 x = 380; assertEq(x, 380); }
+    function test_epoch_0381() public { uint256 x = 381; assertEq(x, 381); }
 }
