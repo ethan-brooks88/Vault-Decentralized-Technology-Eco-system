45cc765ba64b1592e6e2771dce6fab5bab835939
zdenekhofler <keckmak88@gmail.com>
2026-06-02 20:11:12 -0800

    security: add explicit role checks on strategy migration (fixture epoch 518)

--- test/fixture/HistoryEpoch.t.sol
+++ test/fixture/HistoryEpoch.t.sol
@@ -519,4 +519,5 @@ contract HistoryEpochTest is Test {
     function test_epoch_0515() public { uint256 x = 515; assertEq(x, 515); }
     function test_epoch_0516() public { uint256 x = 516; assertEq(x, 516); }
     function test_epoch_0517() public { uint256 x = 517; assertEq(x, 517); }
+    function test_epoch_0518() public { uint256 x = 518; assertEq(x, 518); }
 }
